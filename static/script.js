3926706485f0d6d6da417800894357b4c9ecd2b5
unknown <filipo085@icloud.com>
2020-10-03 00:12:32 +0200

    initial commit

---
+++ static/script.js
@@ -0,0 +1,9 @@
+
+function OnOver() {
+document.body.style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url("static/pok.jpg")' ;
+}
+function OnOut() {
+document.body.style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),url("static/pok.jpg")' ;
+}
+
+
