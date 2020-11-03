c2aa746b2c32644be147c99fb87592ffa5d3d0d6
unknown <filipo085@icloud.com>
2020-11-03 17:03:18 +0100

    siema

--- app.py
+++ app.py
@@ -6,19 +6,19 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, LoginManager, login_required, login_user, current_user, logout_user
 from os import environ
 
-
 app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = 'mysecret'
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL') or 'sqlite:///pokemons.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db= SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 # create login_manager here:
 login_manager = LoginManager()
 login_manager.login_view = 'login'
 # initialize login_manager here:
 login_manager.init_app(app)
+
 
 class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -33,87 +33,79 @@ class Users(UserMixin, db.Model):
     def set_password(self, password):
         self.password = generate_password_hash(password)
 
+
 class Trainers(UserMixin, db.Model):
-    id= db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     trainer_name = db.Column(db.String(100), index=True, unique=True)
     is_good = db.Column(db.String(5))
+
 
 @login_manager.user_loader
 def load_user(id):
     return Users.query.get(int(id))
 
 
-@app.route('/', methods =["GET","POST"])
+@app.route('/', methods=["GET", "POST"])
 def index():
-
     return render_template('index.html')
 
 
-@app.route('/register', methods =["GET","POST"])
+@app.route('/register', methods=["GET", "POST"])
 def register():
-
     return render_template('register.html')
 
 
-@app.route('/game/add_trainer', methods =["GET", "POST"])
+@app.route('/game/add_trainer', methods=["GET", "POST"])
 def trainer_add():
     if request.method == "POST":
-            trainer= Trainers(
-                trainer_name = request.form.get('trainer_name'),
-                is_good = request.form.get('is_good')
-            )
-            db.session.add(trainer)
-            try:
-                db.session.commit()
-            except IntegrityError:
-                db.session.rollback()
-                return redirect(url_for('game_err'))
+        trainer = Trainers(
+            trainer_name=request.form.get('trainer_name'),
+            is_good=request.form.get('is_good')
+        )
+        db.session.add(trainer)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+            return redirect(url_for('game_err'))
 
     return redirect(url_for("game_app"))
 
-@app.route('/game/app', methods =["GET","POST"])
+
+@app.route('/game/app', methods=["GET", "POST"])
 def game_app():
-
-    return render_template('game_app.html')
-
+    return render_template('game_app.html', current_user=user)
 
 
-@app.route('/register/add', methods =["GET", "POST"])
+@app.route('/register/add', methods=["GET", "POST"])
 def register_add():
     if request.method == "POST":
-            user= Users(
-                username = request.form.get('username').lower(),
-                email = request.form.get('email')
-            )
-            user.set_password(request.form.get('password'))
-            db.session.add(user)
-            try:
-                db.session.commit()
-            except IntegrityError:
-                db.session.rollback()
-                return redirect(url_for('register_err'))
-
-
+        user = Users(
+            username=request.form.get('username').lower(),
+            email=request.form.get('email')
+        )
+        user.set_password(request.form.get('password'))
+        db.session.add(user)
+        try:
+            db.session.commit()
+        except IntegrityError:
+            db.session.rollback()
+            return redirect(url_for('register_err'))
     return redirect(url_for("login"))
 
 
-@app.route('/login', methods =["GET","POST"])
+@app.route('/login', methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         user = Users.query.filter_by(username=request.form['username_log']).first()
-
-        if user and user.check_password(request.form['password_log']) :
+        if user and user.check_password(request.form['password_log']):
             login_user(user)
-            return  redirect(url_for('game'))
-
-
-        return  redirect(url_for('login_err'))
-
-
+            return redirect(url_for('game'))
+        return redirect(url_for('login_err'))
     return render_template('login.html')
 
 
-@app.route('/game', methods =["GET","POST"])
+@app.route('/game', methods=["GET", "POST"])
 @login_required
 def game():
     return render_template('game.html')
@@ -122,7 +114,8 @@ def game():
 @login_manager.unauthorized_handler
 def unauthorized():
     # do stuff
-    return "You are not logged in. Click here to get <a href="+ str("/")+">back to Landing Page</a>"
+    return "You are not logged in. Click here to get <a href=" + str("/") + ">back to Landing Page</a>"
+
 
 @app.route("/logout")
 @login_required
@@ -132,16 +125,15 @@ def logout():
 
 
 # ERRORS TEMPLATES
-@app.route('/login/error', methods =["GET","POST"])
+
+@app.route('/login/error', methods=["GET", "POST"])
 def login_err():
     return render_template('error.html')
 
-@app.route('/register/error', methods =["GET","POST"])
+
+@app.route('/register/error', methods=["GET", "POST"])
 def register_err():
     return render_template('integrityError.html')
 
-@app.route('/test', methods =["GET","POST"])
-def test():
-    return render_template('test.html')
 
 
